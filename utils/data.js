a11109a5875b70a0c11f0cc6972cb6773a131415
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-04-06 22:35:09 +1000

    started creating data files to seed data

---
+++ utils/data.js
@@ -0,0 +1,104 @@
+const firstNames = [];
+const lastNames = [
+    'elsher',
+    'solace',
+    'levine',
+    'thatcher',
+    'raven',
+    'bardot',
+    'james',
+    'hansley',
+    'cromwell',
+    'ashley',
+
+11. Monroe
+
+12. West
+
+13. Langley
+
+14. Daughtler
+
+15. Madison
+
+16. Marley
+
+17. Ellis
+
+18. Hope
+
+19. Cassidy
+
+20. Lopez
+
+21. Jenkins
+
+22. Poverly
+
+23. McKenna
+
+24. Gonzales
+
+25. Keller
+
+1. Collymore
+
+2. Stoll
+
+3. Verlice
+
+4. Adler
+
+5. Huxley
+
+6. Ledger
+
+7. Hayes
+
+8. Ford
+
+9. Finnegan
+
+10. Beckett
+
+11. Gatlin
+
+12. Pierce
+
+13. Zimmerman
+
+14. Dawson
+
+15. Wilson
+
+16. Adair
+
+17. Gray
+
+18. Curran
+
+19. Crassus
+
+20. Anderson
+
+21. Adams
+
+22. Carter
+
+23. Hendrix
+
+24. Lennon
+
+25. Gasper
+];
+const emails = [
+    '@gmail.com',
+    '@yahoo.com',
+    '@hotmail.com',
+    '@outlook.com',
+    '@live.com',
+    '@icloud.com',
+    '@mailinator.com',
+    '@mail.com',
+    '@example.com'
+];
