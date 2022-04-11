93e309d65bec02cb36771e2050b943f9cf8ee780
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-04-12 00:17:18 +1000

    got removing reactions working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -86,7 +86,7 @@ module.exports = {
   removeReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
-      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { $pull: { reactions: { _id: req.params.reactionId } } },
       { runValidators: true, new: true }
     )
       .then((thought) =>
